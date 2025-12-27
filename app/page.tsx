8df5e70702abecc65acbb78426c3820e60e196bd
AWAIS_SUFI\awais <awais6javaid@gmail.com>
2025-12-27 15:49:40 +0500

    setup huskey

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ export default function Home() {
   const a = 5;
   console.log(a);
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div className="bg-red text-2xl text-white">Awais ali</div>
+    <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
+      <div className='bg-red text-2xl text-white'>Awais ali</div>
+      <div className='bg-red text-2xl text-white'>Awais ali</div>
+      <div className='bg-red text-2xl text-white'>Awais ali</div>
     </div>
   );
 }
